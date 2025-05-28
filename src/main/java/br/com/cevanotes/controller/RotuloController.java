22e198052ba9c665f31b536df4bb3e25f2c979ab
bernado.prates <bernardo.prates@loft.com.br>
2025-05-28 14:56:57 -0300

    primeiros testes

--- src/main/java/br/com/cevanotes/controller/RotuloController.java
+++ src/main/java/br/com/cevanotes/controller/RotuloController.java
@@ -1,9 +1,20 @@
 package br.com.cevanotes.controller;
 
+import br.com.cevanotes.config.DbConfig;
+import br.com.cevanotes.repository.RotuloRepository;
 import io.javalin.Javalin;
+import io.javalin.http.NotFoundResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class RotuloController {
     public void registrarRotas(Javalin app) {
-        app.get("/rotulos", ctx -> ctx.result("Listagem de rótulos"));
+        try {
+            var repository = new RotuloRepository(DbConfig.createJdbi());
+            app.get("/rotulos", ctx -> ctx.json(repository.findAll()));
+        } catch (SQLException | IOException e) {
+            throw new NotFoundResponse("Nenhum rotulo encontrado");
+        }
     }
 }
---
+++ src/main/java/br/com/cevanotes/repository/RotuloRepository.java
@@ -0,0 +1,22 @@
+package br.com.cevanotes.repository;
+
+import br.com.cevanotes.model.Rotulo;
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.List;
+
+public class RotuloRepository {
+    private final Jdbi jdbi;
+
+    public RotuloRepository(Jdbi jdbi) {
+        this.jdbi = jdbi;
+    }
+
+    public List<Rotulo> findAll() {
+        return jdbi.withHandle(handle ->
+                handle.createQuery("SELECT * FROM rotulos")
+                        .mapToBean(Rotulo.class)
+                        .list()
+        );
+    }
+}
