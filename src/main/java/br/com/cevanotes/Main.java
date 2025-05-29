416a8ffa8f44889268ee327a33264768b6f4e592
bernado.prates <bernardo.prates@loft.com.br>
2025-05-29 18:42:30 -0300

    adicionando um log

--- src/main/java/br/com/cevanotes/Main.java
+++ src/main/java/br/com/cevanotes/Main.java
@@ -6,12 +6,15 @@ import br.com.cevanotes.repository.RotuloRepository;
 import br.com.cevanotes.service.RotuloService;
 import io.javalin.Javalin;
 import io.javalin.http.UnauthorizedResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
     private static final String TOKEN_SECRETO = "meu-token-123";
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws SQLException, IOException {
 
@@ -21,6 +24,8 @@ public class Main {
         var rotuloService = new RotuloService(rotuloRepository);
 
         validacaoDeAcesso(app);
+        app.after(ctx ->
+            log.info("Requisição: {} {} -> status {}", ctx.method(), ctx.path(), ctx.status()));
 
         app.get("/teste", ctx -> ctx.result("API funcionando!"));
         new RotuloController(rotuloService).registrarRotas(app);
